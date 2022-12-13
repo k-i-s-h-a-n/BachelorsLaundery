b657e9e4d95fe4ee9a436fb5f4e6305894cd49d3
k-i-s-h-a-n <102991525+k-i-s-h-a-n@users.noreply.github.com>
2022-12-13 10:18:51 +0530

    Update settings.py

--- Hello/Hello/settings.py
+++ Hello/Hello/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-pq-t!_54vct5x1bt=_3z_pxc-(qsd9=a6(f&70a()*bdi&vb-3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
 
 
 # Application definition
